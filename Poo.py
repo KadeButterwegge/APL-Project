a6543322a154a21113c4d52614cd74b7fbdebcd9
KadeButterwegge <112200024+KadeButterwegge@users.noreply.github.com>
2022-09-02 13:33:23 -0500

    Update Poo.py

--- Poo.py
+++ Poo.py
@@ -11,7 +11,21 @@ while running:
     
     screen.fill((0, 160, 0))
 
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    # Border for level 1
+    pygame.draw.line(screen, (0, 0, 0), (100, 25), (100, 475), 4)
+    pygame.draw.line(screen, (0, 0, 0), (100, 25), (400, 25), 4)
+    pygame.draw.line(screen, (0, 0, 0), (100, 475), (400, 475), 4)
+    pygame.draw.line(screen, (0, 0, 0), (400, 25), (400, 475), 4)
+
+    # Triangle Obstacle for level 1
+    pygame.draw.line(screen, (0, 0, 0), (200, 150), (250, 200), 4)
+    pygame.draw.line(screen, (0, 0, 0), (250, 200), (300, 150), 4)
+    pygame.draw.line(screen, (0, 0, 0), (200, 150), (250, 200), 4)
 
     pygame.display.flip()
+
+    def level1():
+        screen.fill((0, 160, 0))
+
+
 pygame.quit()
