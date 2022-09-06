6d78eb0dfa91a60c55b5c1383fbcadb20484ef81
KadeButterwegge <112200024+KadeButterwegge@users.noreply.github.com>
2022-09-06 13:16:07 -0500

    Update Poo.py

--- Poo.py
+++ Poo.py
@@ -1,7 +1,14 @@
+#Imports
 import pygame
 pygame.init()
 
+#Make Screen
 screen = pygame.display.set_mode([500, 500])
+
+#Icon and Caption
+pygame.display.set_caption("Mini Golf")
+icon = pygame.image.load('Logo.png')
+pygame.display.set_icon(icon)
 
 running = True
 while running:
@@ -21,6 +28,9 @@ while running:
     pygame.draw.line(screen, (0, 0, 0), (200, 150), (250, 200), 4)
     pygame.draw.line(screen, (0, 0, 0), (250, 200), (300, 150), 4)
     pygame.draw.line(screen, (0, 0, 0), (200, 150), (300, 150), 4)
+
+    # Side Triange for level 1
+    pygame.draw.line(screen, (0, 0, 0), (100, 350), (150, 350), 4)
 
     pygame.display.flip()
 
