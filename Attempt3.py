075ad139e8deaed1bf9604349cd1ef025be8d9dc
KadeButterwegge <112200024+KadeButterwegge@users.noreply.github.com>
2022-09-23 12:17:19 -0500

    Update Attempt3.py

--- Attempt3.py
+++ Attempt3.py
@@ -81,51 +81,54 @@ while running:
     clock.tick(FPS)
     pygame.display.flip()
     levelMethods[level-1]()
+    try:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                xMouse = event.pos[0]
+                yMouse = event.pos[1]
+                startPos = [xMouse, yMouse]
+        elif event.type == pygame.MOUSEBUTTONUP and ballrect.collidepoint(startPos[0], startPos[1]):
+            endPos = [event.pos[0], event.pos[1]]
+            xspeed = (startPos[0]-endPos[0])/3
+            yspeed = (startPos[1]-endPos[1])/3
+            if xspeed > 14:
+                xspeed = 14
+            if yspeed > 14:
+                yspeed = 14
+            if xspeed < -14:
+                xspeed = -14
+            if yspeed < -14:
+                yspeed = -14
 
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        if event.button == 1:
-            xMouse = event.pos[0]
-            yMouse = event.pos[1]
-            startPos = [xMouse, yMouse]
-    elif event.type == pygame.MOUSEBUTTONUP and ballrect.collidepoint(startPos[0], startPos[1]):
-        endPos = [event.pos[0], event.pos[1]]
-        xspeed = (startPos[0]-endPos[0])/3
-        yspeed = (startPos[1]-endPos[1])/3
-        if xspeed > 14:
-            xspeed = 14
-        if yspeed > 14:
-            yspeed = 14
-        if xspeed < -14:
-            xspeed = -14
-        if yspeed < -14:
-            yspeed = -14
+            friction = 0.5
 
-        friction = 0.5
+            while abs(xspeed) > 0 or abs(yspeed) > 0:
+                ballrect.move_ip(xspeed, yspeed)
+                time.sleep(.016)
+                screen.blit(ball, ballrect)
+                levelMethods[level-1]()
+                pygame.display.flip()
+                for rectx in levelRects[level-1][0]:
+                    if ballrect.colliderect(rectx):
+                        xspeed = -xspeed
+                for recty in levelRects[level-1][1]:
+                    if ballrect.colliderect(recty):
+                        yspeed = -yspeed
+                if xspeed > 0:
+                    xspeed -= friction
+                if yspeed > 0:
+                    yspeed -= friction
+                if xspeed < 0:
+                    xspeed += friction
+                if yspeed < 0:
+                    yspeed += friction
+                if xspeed > -0.5 and xspeed < 0.5:
+                    xspeed = 0
+                if yspeed > -0.5 and yspeed < 0.5:
+                    yspeed = 0
 
-        while abs(xspeed) > 0 or abs(yspeed) > 0:
-            ballrect.move_ip(xspeed, yspeed)
-            time.sleep(.016)
-            screen.blit(ball, ballrect)
-            levelMethods[level-1]()
-            pygame.display.flip()
-            for rectx in levelRects[level-1][0]:
-                if ballrect.colliderect(rectx):
-                    xspeed = -xspeed
-            for recty in levelRects[level-1][1]:
-                if ballrect.colliderect(recty):
-                    yspeed = -yspeed
-            if xspeed > 0:
-                xspeed -= friction
-            if yspeed > 0:
-                yspeed -= friction
-            if xspeed < 0:
-                xspeed += friction
-            if yspeed < 0:
-                yspeed += friction
-            if xspeed > -0.5 and xspeed < 0.5:
-                xspeed = 0
-            if yspeed > -0.5 and yspeed < 0.5:
-                yspeed = 0
+    except:
+        print("Try again")
 
 
 
