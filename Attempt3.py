e7550a4c23ebb6d21354f853f1f4fcb54b207cad
LRSmidt <112508216+LRSmidt@users.noreply.github.com>
2022-10-13 13:43:19 -0500

    Update Attempt3.py

--- Attempt3.py
+++ Attempt3.py
@@ -29,9 +29,19 @@ pygame.display.set_icon(icon)
 
 #Level layouts
 setPos = True
-level = 1
+level = 3
 levelRects = []
 levelMethods = []
+
+#Level colors
+borderCol = (0, 0, 0)
+grassCol = (0, 150, 0)
+
+#Terrain frictions
+grassFric = 0.93
+boostFric = 1.05
+roughFric = 0.67
+iceFric = .999999
 
         
 
@@ -43,23 +53,27 @@ def level1():
     # Lists for horizontal and vertical rectangles
     Xobj = []
     Yobj = []
+    terrain = []
+    terFric = []
     # Width for the border
     width = 6
     
     # Ground
-    grass = pygame.draw.rect(screen, (0, 160, 0), [100, 25, 300, 425], 0)
+    grass = pygame.draw.rect(screen, grassCol, [100, 25, 300, 425], 0)
+    terrain.append(grass)
+    terFric.append(grassFric)
    
     # Border rectangles
-    Wall1 = pygame.draw.rect(screen, (0, 0, 0), [100, 25, 300, width], 0)
+    Wall1 = pygame.draw.rect(screen, borderCol, [100, 25, 300, width], 0)
     Yobj.append(Wall1)
 
-    Wall2 = pygame.draw.rect(screen, (0, 0, 0), [100, 25, width, 425], 0)
+    Wall2 = pygame.draw.rect(screen, borderCol, [100, 25, width, 425], 0)
     Xobj.append(Wall2)
 
-    Wall3 = pygame.draw.rect(screen, (0, 0, 0), [100, 450, 300, width], 0)
+    Wall3 = pygame.draw.rect(screen, borderCol, [100, 450, 300, width], 0)
     Yobj.append(Wall3)
 
-    Wall4 = pygame.draw.rect(screen, (0, 0, 0), [400, 25, width, 430], 0)
+    Wall4 = pygame.draw.rect(screen, borderCol, [400, 25, width, 430], 0)
     Xobj.append(Wall4)
 
     # 2D array containging vertical and horizontal rectangles lists
@@ -67,6 +81,8 @@ def level1():
         level1Rects = []
         level1Rects.append(Xobj)
         level1Rects.append(Yobj)
+        level1Rects.append(terrain)
+        level1Rects.append(terFric)
         levelRects.append(level1Rects)
     
     # Place hole on screen
@@ -81,60 +97,69 @@ def level2():
     global setPos
     screen.fill((0, 0, 150))
     # Lists for horizontal and vertical rectangles
-    Xobj2 = []
-    Yobj2 = []
+    Xobj = []
+    Yobj = []
+    terrain = []
+    terFric = []
 
     # Ground
-    grass = pygame.draw.rect(screen, (0, 160, 0), [50, 25, 400, 180], 0)
-    grass2 = pygame.draw.rect(screen, (0, 160, 0), [188, 175, 133, 300])
+    grass = pygame.draw.rect(screen, grassCol, [50, 25, 400, 180], 0)
+    terrain.append(grass)
+    terFric.append(grassFric)
+
+    grass2 = pygame.draw.rect(screen, grassCol, [188, 175, 133, 300])
     water = pygame.draw.rect(screen, (0, 0, 150), [130, 80, 245, 70], 0)
+    terrain.append(grass)
+    terFric.append(grassFric)
 
     # Width for the border
     width = 6
 
-    top_wall = pygame.draw.rect(screen, (0, 0, 0), [50, 25, 400, width], 0)
-    Yobj2.append(top_wall)
-
-    top_left = pygame.draw.rect(screen, (0, 0, 0), [50, 25, width, 180], 0)
-    Xobj2.append(top_left)
-
-    mid_left = pygame.draw.rect(screen, (0, 0, 0), [50, 205, 138, width], 0)
-    Yobj2.append(mid_left)
-
-    top_right = pygame.draw.rect(screen, (0, 0, 0), [450, 25, width, 180], 0)
-    Xobj2.append(top_right)
-
-    mid_right = pygame.draw.rect(screen, (0, 0, 0), [326, 205, 130, width], 0)
-    Yobj2.append(mid_right)
-
-    bot_left = pygame.draw.rect(screen, (0, 0, 0), [188, 205, width, 270], 0)
-    Xobj2.append(bot_left)
-
-    bot_right = pygame.draw.rect(screen, (0, 0, 0), [321, 205, width, 270], 0)
-    Xobj2.append(bot_right)
-
-    bot_wall = pygame.draw.rect(screen, (0, 0, 0), [188, 475, 139, width], 0)
-    Yobj2.append(bot_wall)
-
-    top_obs = pygame.draw.rect(screen, (0, 0, 0), [125, 75, 250, width], 0)
-    Yobj2.append(top_obs)
-
-    bot_obs = pygame.draw.rect(screen, (0, 0, 0), [125, 150, 250, width], 0)
-    Yobj2.append(bot_obs)
-
-    left_obs = pygame.draw.rect(screen, (0, 0, 0), [125, 75, width, 75], 0)
-    Xobj2.append(left_obs)
-    
-    right_obs = pygame.draw.rect(screen, (0, 0, 0), [375, 75, width, 81], 0)
-    Xobj2.append(right_obs)
-
     # Border rectangles
+    top_wall = pygame.draw.rect(screen, borderCol, [50, 25, 400, width], 0)
+    Yobj.append(top_wall)
+
+    top_left = pygame.draw.rect(screen, borderCol, [50, 25, width, 180], 0)
+    Xobj.append(top_left)
+
+    mid_left = pygame.draw.rect(screen, borderCol, [50, 205, 138, width], 0)
+    Yobj.append(mid_left)
+
+    top_right = pygame.draw.rect(screen, borderCol, [450, 25, width, 180], 0)
+    Xobj.append(top_right)
+
+    mid_right = pygame.draw.rect(screen, borderCol, [326, 205, 130, width], 0)
+    Yobj.append(mid_right)
+
+    bot_left = pygame.draw.rect(screen, borderCol, [188, 205, width, 270], 0)
+    Xobj.append(bot_left)
+
+    bot_right = pygame.draw.rect(screen, borderCol, [321, 205, width, 270], 0)
+    Xobj.append(bot_right)
+
+    bot_wall = pygame.draw.rect(screen, borderCol, [188, 475, 139, width], 0)
+    Yobj.append(bot_wall)
+
+    top_obs = pygame.draw.rect(screen, borderCol, [125, 75, 250, width], 0)
+    Yobj.append(top_obs)
+
+    bot_obs = pygame.draw.rect(screen, borderCol, [125, 150, 250, width], 0)
+    Yobj.append(bot_obs)
+
+    left_obs = pygame.draw.rect(screen, borderCol, [125, 75, width, 75], 0)
+    Xobj.append(left_obs)
+    
+    right_obs = pygame.draw.rect(screen, borderCol, [375, 75, width, 81], 0)
+    Xobj.append(right_obs)
 
     # 2D array containging vertical and horizontal rectangles lists
-    level2Rects = []
-    level2Rects.append(Xobj2)
-    level2Rects.append(Yobj2)
-    levelRects.append(level2Rects)
+    if len(levelRects) <= level-1:
+        level2Rects = []
+        level2Rects.append(Xobj)
+        level2Rects.append(Yobj)
+        level2Rects.append(terrain)
+        level2Rects.append(terFric)
+        levelRects.append(level2Rects)
     
     # Place hole on screen
     holerect.update((250, 40), (30, 30))
@@ -142,12 +167,74 @@ def level2():
 
     # Start position for the ball
     if setPos:
-        ballrect.update((250, 400), (14, 14))
+        ballrect.update((250, 400), (30, 30))
         setPos = False
     screen.blit(ball, ballrect)
 
+def level3():
+    global levelRects
+    screen.fill((0, 0, 150))
+    # Lists for horizontal and vertical rectangles
+    Xobj = []
+    Yobj = []
+    terrain = []
+    terFric = []
+    # Width for the border
+    width = 6
+    
+    # Ground
+    grass = pygame.draw.rect(screen, grassCol, [50, 50, 400, 250])
+    terrain.append(grass)
+    terFric.append(grassFric)
+
+    water = pygame.draw.rect(screen, (0, 0, 150), [127, 120, 255, 115])
+
+    # Border rectangles
+    top = pygame.draw.rect(screen, borderCol, [50, 50, 400, width])
+    Yobj.append(top)
+
+    topL = pygame.draw.rect(screen, borderCol, [50, 50, width, 250])
+    Xobj.append(topL)
+
+    topR = pygame.draw.rect(screen, borderCol, [450, 50, width, 250])
+    Xobj.append(topR)
+
+    bot = pygame.draw.rect(screen, borderCol, [50, 300, 406, width])
+    Yobj.append(bot)
+
+    #Obstacle rectangles
+    obsT = pygame.draw.rect(screen, borderCol, [122, 115, 260, width])
+    Yobj.append(obsT)
+
+    obsL = pygame.draw.rect(screen, borderCol, [122, 115, width, 120])
+    Xobj.append(obsL)
+
+    obsR = pygame.draw.rect(screen, borderCol, [382, 115, width, 120])
+    Xobj.append(obsR)
+
+    obsB = pygame.draw.rect(screen, borderCol, [122, 235, 266, width])
+    Yobj.append(obsB)
+
+    # 2D array containging vertical and horizontal rectangles lists
+    if len(levelRects) <= level-1:
+        level3Rects = []
+        level3Rects.append(Xobj)
+        level3Rects.append(Yobj)
+        level3Rects.append(terrain)
+        level3Rects.append(terFric)
+        levelRects.append(level3Rects)
+    
+    # Place hole on screen
+    #holerect.update((250, 50), (30, 30))
+    #screen.blit(hole, holerect)
+
+    # Start position for the ball
+    screen.blit(ball, ballrect)
+
+
 levelMethods.append(level1)
 levelMethods.append(level2)
+levelMethods.append(level3)
 
 running = True
 doodoo = True
@@ -186,14 +273,19 @@ while running:
                 #Draw level
                 levelMethods[level-1]()
                 pygame.display.flip()
-
+                friction = 0.93
                 #Check for wall collisions
                 for rectx in levelRects[level-1][0]:
                     if ballrect.colliderect(rectx):
                         xspeed = -xspeed
                 for recty in levelRects[level-1][1]:
                     if ballrect.colliderect(recty):
-                        yspeed = -yspeed
+                        yspeed = -yspeed                        
+                index = 0
+                for rect in levelRects[level-1][2]:
+                    if rect.contains(ballrect):
+                        friction = levelRects[level-1][3][index]
+                        index += 1
                 #Slow down the ball
                 if xspeed > 0:
                     xspeed *= friction
@@ -224,4 +316,4 @@ while running:
         poo = 123123123123123
 
 #
-pygame.quit()
+pygame.quit()   
